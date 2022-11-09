cf996fd75d3ba9a1c666b49d24c0933d1173565f
tobiasgoncalvespires <tobiasgp@gmail.com>
2022-11-09 18:30:48 -0300

    Sending algorithm

--- trellis/api/util/num_to_english_util.py
+++ trellis/api/util/num_to_english_util.py
@@ -1,2 +1,96 @@
-def to_english(value):
-    return 'num is ' + str(value)
+ref = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+    100: 'one hundred'
+}
+
+h = 100
+k = 1000
+m = k * k
+b = m * k
+t = b * k
+q = t * k
+z = q * k
+s = z * k
+
+labels = {
+    h: 'hundred',
+    k: 'thousand',
+    m: 'million',
+    b: 'billion',
+    t: 'trillion',
+    q: 'quadrillion',
+    z: 'quintillion',
+    s: 'sextillion'
+}
+
+
+def get_previous_scale(scale):
+    if scale == k:
+        return h
+    return int(scale / k)
+
+
+def get_greater_and_label():
+    greater = list(labels.keys())[-1]
+    label = labels[greater]
+    return greater, label
+
+
+def to_english(num):
+    if num < 0:
+        raise Exception('numbers less than 0 are not allowed')
+
+    max_number, label = get_greater_and_label()
+    if num > max_number:
+        raise Exception('numbers greater than one ' + label + ' are not allowed')
+
+    if num in ref:
+        return ref[num]
+
+    if num < h:
+        if num % 10 == 0:
+            return ref[num]
+
+        return ref[num // 10 * 10] + '-' + ref[num % 10]
+
+    for scale in labels.keys():
+        if not num < scale:
+            continue
+
+        p_scale = get_previous_scale(scale)
+        label = labels[p_scale]
+
+        partial_result = to_english(num // p_scale) + ' ' + label + ' '
+        if num % p_scale == 0:
+            return partial_result
+
+        return partial_result + to_english(num % p_scale)
+
+    raise Exception('samething is wrong')
--- trellis/settings.py
+++ trellis/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pde5=h_)eb^89xbf-dudq0g3dj^xih0$a^yxf(fmqw0p@cx9-q'
+SECRET_KEY = os.getenv('TRELLIS_DJANGO_SECRET_KEY', 'temp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
